50bc16ceaecb9abdc2a79dbfe1a05d28377ac913
Andy Yong Weng Zhin <andyywz@gmail.com>
2013-08-09 09:26:56 -0400

    Adds some changes to the header.

--- app/assets/javascripts/insert-event.js
+++ app/assets/javascripts/insert-event.js
@@ -1,9 +1,9 @@
-var createDatestamp = function (date, time) {
-	
-	
-	var dateTime = date + "T" + newtime;
-	var timeStamp = Date.parse(dateTime);
-}
+// var createDatestamp = function (date, time) {
+// 	
+// 	
+// 	var dateTime = date + "T" + newtime;
+// 	var timeStamp = Date.parse(dateTime);
+// }
 
 $(document).ready(function () {	
 	$("#event-form").find('input[type="submit"]').on(
@@ -32,16 +32,16 @@ $(document).ready(function () {
 								
 								var date = $(that.form).find("#occurrence_date").val();
 								var time = $(that.form).find("#occurrence_time").val();
-								// var dateTime = date + "T" + time;
-								// var timeStamp = Date.parse(dateTime);
+								var dateTime = date + "T" + time;
+								var timeStamp = Date.parse(dateTime);
 								
-								var dateStamp = createDatestamp(date, time);
+								// var dateStamp = createDatestamp(date, time);
 								
 								var domDate = $(domEl).find(".event-time").attr("data-datestamp") * 1000;
 								
 								if (domDate > timeStamp) {
 									var realDate = new Date(timeStamp);
-									var dateString = realDate.toDateString() + " " + realDate.toLocaleTimeString("en-US",{hour: "numeric", minute: "numeric" })
+									var dateString = realDate.toDateString() + " " + realDate.toLocaleTimeString("en-US", {timeZone: "UTC", hour: "numeric", minute: "numeric"})
 									
 									var prependData = "<li class='event'>                         \
 										<div class='event-title group'>                             \
@@ -97,7 +97,7 @@ $(document).ready(function () {
 								
 								if (domDate > timeStamp) {
 									var realDate = new Date(timeStamp);
-									var dateString = realDate.toDateString() + " " + realDate.toLocaleTimeString("en-US",{hour: "numeric", minute: "numeric" })
+									var dateString = realDate.toDateString() + " " + realDate.toLocaleTimeString("en-US", {timeZone: "UTC", hour: "numeric", minute: "numeric"})
 									
 									var prependData = "<li class='event'>                         \
 										<div class='event-title group'>                             \
