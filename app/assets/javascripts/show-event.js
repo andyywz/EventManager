a19e74eee9e2e0033b5900728b14ab62eba80364
Andy Yong Weng Zhin <andyywz@gmail.com>
2013-08-12 15:46:05 -0400

    Attending events and created events buttons on the profile nav bar works.

--- app/assets/javascripts/show-event.js
+++ app/assets/javascripts/show-event.js
@@ -1,9 +1,25 @@
 $(document).ready(function () {
 	$(".post-content").on("ajax:success", ".occurrence-show", function (event, data) {
-		$("article.post").html(data);
+		$(event.target).find("article.post").html(data);
 	});
 	
-	$(".post-content").on("click", ".attending-events-button", function (event) {
-		$
-	})
+	$(".post-content").on("ajax:success", ".event-show", function (event, data) {
+		$(event.target).find("article.post").html(data);
+	});
+	
+	$(".profile-nav").on("click", ".attending-events-button", function () {
+		if ($("article.post.created").is(":visible")) {
+			$("article.post.created").slideToggle();
+		};
+		
+		$("article.post.attending").slideToggle();
+	});
+	
+	$(".profile-nav").on("click", ".created-events-button", function () {
+		if ($("article.post.attending").is(":visible")) {
+			$("article.post.attending").slideToggle();
+		};
+		
+		$("article.post.created").slideToggle();
+	});
 });
