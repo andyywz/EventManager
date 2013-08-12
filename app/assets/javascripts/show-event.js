a5eaa27698eadff3d69cc9c6bf6f5f965285a80d
Andy Yong Weng Zhin <andyywz@gmail.com>
2013-08-12 16:28:09 -0400

    Adds scroll effect to created events and adds ajax call to get occurrence show page.

--- app/assets/javascripts/show-event.js
+++ app/assets/javascripts/show-event.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
-	$(".post").on("ajax:success", ".occurrence-show", function (event, data) {
+	$(".content-main").on("ajax:success", ".occurrence-show", function (event, data) {
 		$("article.post-event").html(data);
+		
+		if ($("article.post.created").is(":visible")) {
+			$("article.post.created").slideToggle();
+		};
+		
+		if ($("article.post.attending").is(":visible")) {
+			$("article.post.attending").slideToggle();
+		};
 	});
 	
-	$(".post").on("ajax:success", ".event-show", function (event, data) {
+	$(".content-main").on("ajax:success", ".event-show", function (event, data) {
 		$("article.post-event").html(data);
+		
+		if ($("article.post.created").is(":visible")) {
+			$("article.post.created").slideToggle();
+		};
+		
+		if ($("article.post.attending").is(":visible")) {
+			$("article.post.attending").slideToggle();
+		};
 	});
 	
 	$(".profile-nav").on("click", ".attending-events-button", function () {
